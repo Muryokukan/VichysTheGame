b845fb8189bdeee287459f6093521140cc5a73b8
Sarah-Katz <Sarahkatz59@gmail.com>
2024-12-12 20:02:32 +0100

    feat: Implemented round robin tournament logic

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,41 +3,41 @@
 namespace App\Controller;
 
 use App\Entity\Strategies\Collaborator;
+use App\Entity\Strategies\Nazi;
+use App\Entity\Strategies\Resistant;
 use App\Entity\Strategies\Undecided;
-use App\Service\DuelService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Service\TournamentService;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(DuelService $duelService): Response
+    public function index(TournamentService $tournamentService): Response
     {
-        // Create two strategies
-        $strategy0 = new Collaborator();
-        $strategy1 = new Undecided();
+        // TODO: Implement strategies choice by user
+        $chosenStrategies = [
+            new Collaborator(),
+            new Undecided(),
+            new Nazi(),
+            new Resistant(),
+        ];
 
-        $strategies = [$strategy0, $strategy1];
-        // Simulate a duel
-        $choices = $duelService->iterate($strategies);
-        $scores  = $duelService->evaluate($choices);
-
-        $results = [];
-        foreach ($strategies as $index => $strategy) {
-            $strategyKey = "strategy$index";
-            $results[]   = [
-                'name'    => $strategy->getName(),
-                'choices' => $choices[$strategyKey],
-                'score'   => $scores[$strategyKey],
-            ];
+        // Prepare user given strategies for use in the tournamentService
+        $strategies = [];
+        for ($i = 0; $i < count($chosenStrategies); $i++) {
+            // Assign unique names for each strategy to avoid conflicts in the tournamentService
+            // eg: "Collaborator 0", "Undecided 1", ...
+            $strategies[$chosenStrategies[$i]->getName() . " $i"] = $chosenStrategies[$i];
         }
 
-        // dd($results);
+        // Conduct tournament
+        $tournamentResults = $tournamentService->conductTournament($strategies);
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'results'         => $results,
+            'controller_name'   => 'HomeController',
+            'tournamentResults' => $tournamentResults,
         ]);
     }
 }
